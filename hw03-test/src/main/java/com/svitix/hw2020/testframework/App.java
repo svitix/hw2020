c69217e18bd1b8895c0c13c6068098772115ea53
Vitaliy Slutskikh <VSlutskikh@luxoft.com>
2021-02-10 15:26:38 +0600

    fix git problem

---
+++ hw03-test/src/main/java/com/svitix/hw2020/testframework/App.java
@@ -0,0 +1,9 @@
+package com.svitix.hw2020.testframework;
+
+import com.svitix.hw2020.testframework.myjunit.MyJUnit;
+
+public class App {
+    public static void main(String ...args)  {
+        MyJUnit.test(ExampleMyJUnitTest.class);
+    }
+}
